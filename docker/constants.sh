7b61e32a5800e6a8504352773cf1467061e5fb53
João Rocha <joaorosilva@gmail.com>
2019-02-12 23:44:16 +0000

    Migrating docker images to Docker Hub LIAAD counterparts of feupinfolab Removed dependency on requirements.txt from the feup-infolab repository

--- docker/constants.sh
+++ docker/constants.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 YAKE_PORT=5000
-YAKE_IMAGE="feupinfolab/yake"
-YAKE_SERVER_IMAGE="feupinfolab/yake-server"
+YAKE_IMAGE="liaad/yake"
+YAKE_SERVER_IMAGE="liaad/yake-server"
 TAG="latest"
