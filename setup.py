c126e613f20cb1c3dcc4b25c6e3059a60092ed19
Arian Pasquali <arianpasquali@gmail.com>
2019-03-25 19:21:34 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='yake',
-    version='0.4.0',
+    version='0.4.1',
     description="Keyword extraction Python package",
     long_description=readme,
     
