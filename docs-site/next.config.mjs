b01fa15ea55c9e806e5c7b334492e4f7be28c5eb
Tiago Valente <95642257+tiagolv@users.noreply.github.com>
2025-05-05 17:15:08 +0100

    style

--- docs-site/next.config.mjs
+++ docs-site/next.config.mjs
@@ -6,6 +6,8 @@ const withMDX = createMDX();
 const config = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/docs-site',         // nome do repositório
+  assetPrefix: '/docs-site',      // garante que os estilos/scripts carregam corretamente
 };
 
 export default withMDX(config);
